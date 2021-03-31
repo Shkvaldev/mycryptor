fafe160a09350674b9631a64aa8cecd42a2931b5
Shkvaldev <svetlyak1033@gmail.com>
2021-03-31 07:45:37 -0400

    bug fix + aes-256-cbc algorithm

---
+++ README.md
@@ -0,0 +1,14 @@
+MyCryptor
+It's simple python script which uses openssl library to encrypt/decrypt file.
+To run it:
+pip install -r requirements.txt 
+python3 main.py 
+
+or
+
+python main.py
+
+Features:
+- Encryption little files using 2 cryptograpthy algorithms
+- It's easy to use !
+- Algorithms: aes-256-cbc, rsa
--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def comList():
 	comlist.add_column("[italic bold blue]Name[/italic bold blue]", justify="center")
 	comlist.add_column("[italic bold blue]Description[/italic bold blue]", justify="right")
 	#data
-	comlist.add_row("1", "Encryption", "It's just encrypts little files")
-	comlist.add_row("2", "Dencryption", "It's just dencrypts little files")
+	comlist.add_row("1", "Encryption using RSA", "It's just encrypts little files")
+	comlist.add_row("2", "Dencryption using RSA", "It's just dencrypts little files")
+	comlist.add_row("3", "Encryption using AES-256-CBC", "It's just encrypts little files")
+	comlist.add_row("4", "Dencryption using AES-256-CBC", "It's just dencrypts little files")
 	console.print(comlist)
 
 greeting()
@@ -36,13 +38,26 @@ while True:
 		os.system("openssl rsa -in priv.key -pubout -out pub.key")
 		console.print("Public key has generated", style=processStyle, justify="left")
 		os.system("openssl rsautl -encrypt -pubin -inkey pub.key -in {} -out encrypted".format(file))
-		console.print("File has been encrypted", style=processStyle, justify="left")
+		console.print("File has been encrypted with RSA", style=processStyle, justify="left")
 		os.system("cp -r priv.key pub.key encrypted {}".format(name))
 		os.system("rm -rf priv.key pub.key encrypted")
 	if com == "2":
 		name = console.input("Enter enviroment [i bold red]name[/i bold red]: ")
-		os.system("openssl rsautl -dencrypt -inkey {}/priv.key -in {}/encrypted -out dencrypted.txt")
-		console.print("File has been decrypted", style=processStyle, justify="left")
+		os.system("openssl rsautl -decrypt -inkey {}/priv.key -in {}/encrypted -out {}/dencrypted".format(name, name, name))
+		console.print("File has been decrypted with RSA", style=processStyle, justify="left")
+	if com == "3":
+		name = console.input("Enter enviroment [i bold red]name[/i bold red]: ")
+		file = console.input("Enter [i bold red]file path[/i bold red]: ")
+		iter = console.input("Enter [i bold red]number of iteration[/i bold red]: ")
+		os.system("mkdir {}".format(name))
+		os.system("openssl enc -aes-256-cbc -in {} -out encrypted -iter {}".format(file, iter))
+		os.system("cp -r encrypted {} && rm -rf encrypted".format(name))
+		console.print("File has been encrypted with aes-256-cbc", style=processStyle, justify="left")
+	if com == "4":
+		name = console.input("Enter enviroment [i bold red]name[/i bold red]: ")
+		iter = console.input("Enter [i bold red]number of iteration[/i bold red]: ")
+		os.system("openssl enc -aes-256-cbc -d -in {}/encrypted -out {}/decrypted -iter {}".format(name, name, iter))
+		console.print("File has been decrypted with aes-256-cbc", style=processStyle, justify="left")
 	if com == "q":
 		console.print("Bye ...", style=processStyle, justify="left")
 		sys.exit()
